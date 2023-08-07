618865cc1fed6214a82fa8b003dd087b35c91fd0
Siarhei Verkhavodka <siarhei.verkhavodka@leverx.com>
2023-08-07 12:36:15 +0300

    test

--- src/tests/test_all.py
+++ src/tests/test_all.py
@@ -15,6 +15,7 @@ class TestOne:
             resp = requests.get(url="https://www.python.org/")
             self.logger.debug(f"Get status code is {resp.status_code}")
             assert resp.status_code == 201
+            
 
     @pytest.mark.smoke
     def test_two(self):
